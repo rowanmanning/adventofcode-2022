6ab0889887a6811d2a842ab0a0419332ac4de655
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2022-12-04 09:18:58 +0000

    Complete day 1 part 1

--- solutions/day-01/index.js
+++ solutions/day-01/index.js
@@ -1,10 +1,28 @@
 'use strict';
 
-exports.solution1 = function solution1() {
-
+exports.solution1 = function solution1(input) {
+	return Math.max(
+		...input
+			.split('\n\n')
+			.map(elf => {
+				return elf
+					.split('\n')
+					.reduce(sum);
+			})
+	);
 };
 
 exports.solution2 = function solution2() {
 
 };
 
+/**
+ * Add two numbers together.
+ *
+ * @param {string | number} number1 - The first number.
+ * @param {string | number} number2 - The number to add to the first.
+ * @returns {number} - Returns the result of the sum.
+ */
+function sum(number1, number2) {
+	return Number(number1) + Number(number2);
+}
--- solutions/day-01/test/index.test.js
+++ solutions/day-01/test/index.test.js
@@ -1,18 +1,36 @@
 'use strict';
 
 const {assert} = require('chai');
+const {readFile} = require('node:fs/promises');
 
 const {solution1, solution2} = require('..');
 
 describe('solution', () => {
 
 	describe('.solution1(input)', () => {
+		let exampleInput;
+		let puzzleInput;
 
-		beforeEach(() => {
-			solution1();
+		before(async () => {
+			exampleInput = await readFile(`${__dirname}/fixtures/example-input.txt`, 'utf-8');
+			puzzleInput = await readFile(`${__dirname}/fixtures/puzzle-input.txt`, 'utf-8');
 		});
 
-		it('has tests');
+		describe('with test data', () => {
+
+			it('returns the expected result', () => {
+				assert.strictEqual(solution1(exampleInput), 24000);
+			});
+
+		});
+
+		describe('with puzzle input', () => {
+
+			it('returns the expected result', () => {
+				assert.strictEqual(solution1(puzzleInput), 64929);
+			});
+
+		});
 
 	});
 
