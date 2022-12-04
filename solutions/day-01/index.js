ad677351a0772886f6a6d0e083cb197f8fe28261
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2022-12-04 09:00:50 +0000

    Initial commit

---
+++ solutions/day-01/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.solution1 = function solution1() {
+
+};
+
+exports.solution2 = function solution2() {
+
+};
+
---
+++ solutions/day-01/test/index.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {assert} = require('chai');
+
+const {solution1, solution2} = require('..');
+
+describe('solution', () => {
+
+	describe('.solution1(input)', () => {
+
+		beforeEach(() => {
+			solution1();
+		});
+
+		it('has tests');
+
+	});
+
+	describe('.solution2(input)', () => {
+
+		beforeEach(() => {
+			solution2();
+		});
+
+		it('has tests');
+
+	});
+
+});
